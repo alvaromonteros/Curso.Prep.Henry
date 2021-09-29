74667270a071bc7c5435b93a02eb53b19f86e87b
alvaromonteros <alvarom_adm@hotmail.com>
2021-09-29 20:00:42 -0300

    Homework 4 en proceso

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -1,24 +1,32 @@
 // No cambies los nombres de las funciones.
 
 function devolverPrimerElemento(array) {
+  return array [0];
+
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
 }
 
 
 function devolverUltimoElemento(array) {
+  return array [array.length - 1];
   // Devuelve el último elemento de un array
   // Tu código:
 }
 
 
 function obtenerLargoDelArray(array) {
+  return array.length;
   // Devuelve el largo de un array
   // Tu código:
 }
 
 
 function incrementarPorUno(array) {
+  for (var i = 0; i < array.length; i++) {
+    array[i] = array[i] + 1;
+  }
+  return array;
   // "array" debe ser una matriz de enteros (int/integers)
   // Aumenta cada entero por 1
   // y devuelve el array
@@ -27,6 +35,8 @@ function incrementarPorUno(array) {
 
 
 function agregarItemAlFinalDelArray(array, elemento) {
+  array.push (elemento);
+  return array;
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
@@ -34,6 +44,8 @@ function agregarItemAlFinalDelArray(array, elemento) {
 
 
 function agregarItemAlComienzoDelArray(array, elemento) {
+  array.unshift (elemento);
+  return array;
   // Añade el "elemento" al comienzo del array
   // y devuelve el array
   // Pista: usa el método `.unshift`
@@ -42,6 +54,7 @@ function agregarItemAlComienzoDelArray(array, elemento) {
 
 
 function dePalabrasAFrase(palabras) {
+  return palabras.join (' ');
   // "palabras" es un array de strings/cadenas
   // Devuelve un string donde todas las palabras estén concatenadas
   // con espacios entre cada palabra
@@ -51,6 +64,7 @@ function dePalabrasAFrase(palabras) {
 
 
 function arrayContiene(array, elemento) {
+  return array.includes (elemento);
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
@@ -58,6 +72,11 @@ function arrayContiene(array, elemento) {
 
 
 function agregarNumeros(numeros) {
+  var suma = 0;
+  for (var i = 0; i < numeros.length; i++) {
+    suma += numeros[i];
+  }
+  return suma;
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
@@ -65,6 +84,11 @@ function agregarNumeros(numeros) {
 
 
 function promedioResultadosTest(resultadosTest) {
+  var promedio = 0;
+  for (var i = 0; i < resultadosTest.length; i++) {
+    promedio += resultadosTest[i];
+  }
+  return promedio / resultadosTest.length;
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
@@ -72,13 +96,29 @@ function promedioResultadosTest(resultadosTest) {
 
 
 function numeroMasGrande(numeros) {
+  var mayor = numeros [0];
+  for (var i= 1; i < numeros.length; i++) {
+    if (numeros[i] > mayor ) {
+      mayor = numeros [i];
+    }
+  }
+  return mayor;
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
 }
 
 
-function multiplicarArgumentos() {
+function multiplicarArgumentos() { 
+ if (arguments.length === 0) {
+   return 0;
+ }
+ var multi = 1;
+for (var i = 0; i < arguments.length; i++) {
+  multi *= arguments[i];
+}
+return multi;
+
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
@@ -86,6 +126,13 @@ function multiplicarArgumentos() {
 
 
 function cuentoElementos(arreglo){
+  var elementos = [];
+  for (var i = 0; i < arreglo.length; i++) {
+    if ( arreglo[i] > 18) {
+      elementos.push(arreglo[i]); 
+    }
+  }
+  return elementos.length;
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
 
@@ -93,6 +140,10 @@ function cuentoElementos(arreglo){
 
 
 function diaDeLaSemana(numeroDeDia) {
+  if (numeroDeDia === 1 || numeroDeDia === 7) {
+    return 'Es fin de semana';
+  }
+  return 'Es dia Laboral';
   //Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente. 
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
@@ -102,6 +153,11 @@ function diaDeLaSemana(numeroDeDia) {
 
 
 function empiezaConNueve(n) {
+  var num = n.toString ()
+  if (num [0] === '9') {
+    return true;
+  }
+  return false;
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
@@ -110,6 +166,13 @@ function empiezaConNueve(n) {
 
 
 function todosIguales(arreglo) {
+  var igual = arreglo[0]; 
+  for (var i = 1; i < arreglo.length; i++) {
+    if (igual != arreglo[i]) {
+      return false;
+    }
+  }
+  return true;
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
@@ -118,6 +181,23 @@ function todosIguales(arreglo) {
 
 
 function mesesDelAño(array) {
+  for (var j = 0; j < array.length; j++) {
+    if (array[j] !== 'Enero' && array[j] !== 'Marzo' && array[j] !== 'Noviembre') {
+      return 'No se encotraron los meses pedidos';
+    }
+  }
+  var meses = [];
+  for (var i = 0; i < array.length; i++) {
+    switch (array[i]) {
+      case 'Enero':
+        meses.push (array[i]);
+      case 'Marzo':
+        meses.push (array[i]);
+      case 'Noviembre':
+        meses.push (array[i]);
+    }
+  }
+  return meses;
   //Dado un array que contiene algunos meses del año desordenados, recorrer el array buscando los meses de 
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
